2b8f075804b2dbb312ace7db60330bdc4afbbb41
Nick Lay <layns@mail.uc.edu>
2022-11-09 18:55:06 -0500

    implement Medicine

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,28 @@
 package edu.uc.cs3003.medava;
 
 public class Medicine {
+    public Medicine(String medicineName) {
+        mMedicineName = medicineName;
+    }
 
+    private String mMedicineName;
+
+    public String getMedicineName() {
+        return mMedicineName;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }
 }
