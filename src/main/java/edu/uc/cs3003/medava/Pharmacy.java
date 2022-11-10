0a89febc59c6c82ae1405481c7f6995d48487f7e
Nick Lay <layns@mail.uc.edu>
2022-11-09 22:04:10 -0500

    IMPLEMENT OXY, add schedule control

--- src/main/java/edu/uc/cs3003/medava/Pharmacy.java
+++ src/main/java/edu/uc/cs3003/medava/Pharmacy.java
@@ -4,14 +4,13 @@ public class Pharmacy {
     public Pharmacy(String pharmacyName) {
         mPharmacyName = pharmacyName;
     }
-    
 
     public String pharmacyName() {
         return mPharmacyName;
     }
 
     private String mPharmacyName;
-    
+
     public boolean send(Transporter t) {
         Medicine advil = new Ibuprofen();
         if (t.load(advil)) {
@@ -32,7 +31,18 @@ public class Pharmacy {
                     t.getTransporterName()));
             return false;
         }
+
+        Medicine oxycontin = new Oxycodone();
+        if (t.load(oxycontin)) {
+            System.out.println(String.format("Sending %s on the %s transporter.", oxycontin.getMedicineName(),
+                    t.getTransporterName()));
+        } else {
+            System.out.println(String.format("Cannot load %s on to the %s transporter.", oxycontin.getMedicineName(),
+                    t.getTransporterName()));
+            return false;
+        }
+
         return true;
-    } 
-    
+    }
+
 }
