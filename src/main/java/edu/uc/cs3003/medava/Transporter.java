ab14b3c6dee6902dfabafce163baf12e68e67994
Nick Lay <layns@mail.uc.edu>
2022-11-09 19:16:08 -0500

    implement Hospital/Transporter

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,21 @@
 package edu.uc.cs3003.medava;
+
+import java.util.ArrayList;
+
 public class Transporter {
+    { goods = new ArrayList<Medicine>() {
+        
+    };}
+    
+    private String mTransporterName;
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    public ArrayList<Medicine> goods;
+
+    public void ship() {
+        // Do some shipping!
+    }
 }
